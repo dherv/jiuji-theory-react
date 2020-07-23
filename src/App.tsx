23a148b99c1121d205e62b844b3a79b90699da34
dherv <damien.herv@gmail.com>
2020-07-24 08:51:44 +0900

    feat: init commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import "destyle.css";
+import GlobalStyle from "./styled/global";
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+    </>
+  );
+}
+
+export default App;
