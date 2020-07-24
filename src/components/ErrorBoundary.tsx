01f815e33a586d4fb0f3b3b1a1a54124b6dd2c50
dherv <damien.herv@gmail.com>
2020-07-24 13:10:22 +0900

    chore(error): add ErrorBoundary and BugSnag connection for the logs

---
+++ src/components/ErrorBoundary.tsx
@@ -0,0 +1,28 @@
+import React, { FC } from "react";
+
+import Bugsnag from "@bugsnag/js";
+import BugsnagPluginReact from "@bugsnag/plugin-react";
+
+Bugsnag.start({
+  apiKey: process.env.REACT_APP_BUGSNAG_API_KEY!,
+  plugins: [new BugsnagPluginReact()],
+});
+
+// Handle Typescript on plugin with !
+const BugSnagErrorBoundary = Bugsnag.getPlugin("react")!.createErrorBoundary(
+  React
+);
+
+const FallbackComponent = () => {
+  return <h1>an Error occured in the Boundaries!</h1>;
+};
+
+const ErrorBoundary: FC<{}> = ({ children }) => {
+  return (
+    <BugSnagErrorBoundary FallbackComponent={FallbackComponent}>
+      {children}
+    </BugSnagErrorBoundary>
+  );
+};
+
+export default ErrorBoundary;
