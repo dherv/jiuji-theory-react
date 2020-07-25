a40dbdd951fc19bd140d7ec0d0cb493678a28630
dherv <damien.herv@gmail.com>
2020-07-25 13:43:26 +0900

    feat(form): add radio buttons

--- src/components/FormCreateEdit.tsx
+++ src/components/FormCreateEdit.tsx
@@ -3,6 +3,7 @@ import { Formik, Field, Form, FormikHelpers } from "formik";
 import FormField from "./FormField";
 import styled from "styled-components";
 import Button from "./Button";
+import { themeBlueLight, themeBlue } from "../styled/themes";
 
 interface Values {
   firstName: string;
@@ -11,12 +12,14 @@ interface Values {
 }
 
 const FormCreateEdit: FC<{}> = () => {
+  const teachers = [{ name: "Tom" }, { name: "John" }];
   return (
     <Formik
       initialValues={{
         firstName: "",
         lastName: "",
         email: "",
+        teacher: "",
       }}
       onSubmit={(values: Values, { setSubmitting }: FormikHelpers<Values>) => {
         setTimeout(() => {
@@ -25,32 +28,88 @@ const FormCreateEdit: FC<{}> = () => {
         }, 200);
       }}
     >
-      <Form>
-        <FormBlock>
-          <FormLabel htmlFor="firstName">First Name</FormLabel>
-          <FormField id="firstName" name="firstName" placeholder="John" />
-        </FormBlock>
+      {({ values }) => (
+        <Form>
+          <FormBlock>
+            <FormLabel htmlFor="name">name</FormLabel>
+            <FormField id="name" name="name" placeholder="name" />
+          </FormBlock>
 
-        <FormBlock>
-          <FormLabel htmlFor="lastName">Last Name Name</FormLabel>
-          <FormField id="lastName" name="lastName" placeholder="John" />
-        </FormBlock>
+          <FormBlock>
+            {teachers.map((teacher) => (
+              <>
+                <FormRadioButton
+                  type="button"
+                  theme={
+                    values.teacher === teacher.name ? themeBlue : themeBlueLight
+                  }
+                >
+                  <label
+                    style={{ display: "block", padding: ".5rem 1rem" }}
+                    htmlFor={teacher.name}
+                  >
+                    {teacher.name}
+                  </label>
+                </FormRadioButton>
 
-        <FormBlock>
-          <FormLabel htmlFor="email">Email</FormLabel>
-          <FormField
-            id="email"
-            name="email"
-            placeholder="john@acme.com"
-            type="email"
-          />
-        </FormBlock>
+                <Field
+                  type="radio"
+                  value={teacher.name}
+                  name="teacher"
+                  id={teacher.name}
+                  style={{ display: "none" }}
+                ></Field>
+              </>
+            ))}
+          </FormBlock>
 
-        <Button type="submit">Submit</Button>
-      </Form>
+          {values.teacher}
+
+          <FormBlock>
+            <FormLabel htmlFor="position">position</FormLabel>
+            <Field component="select" id="position" name="position">
+              <option value="NY">New York</option>
+              <option value="SF">San Francisco</option>
+              <option value="CH">Chicago</option>
+              <option value="OTHER">Other</option>
+            </Field>
+          </FormBlock>
+
+          <FormBlock>
+            <FormLabel htmlFor="lastName">categorie</FormLabel>
+            <FormField id="lastName" name="lastName" placeholder="John" />
+          </FormBlock>
+
+          <FormBlock>
+            <FormLabel htmlFor="lastName">technique</FormLabel>
+            <FormField id="lastName" name="lastName" placeholder="John" />
+          </FormBlock>
+
+          <FormBlock>
+            <FormLabel htmlFor="lastName">steps</FormLabel>
+            <FormField id="lastName" name="lastName" placeholder="John" />
+          </FormBlock>
+
+          <FormBlock>
+            <FormLabel htmlFor="email">Email</FormLabel>
+            <FormField
+              id="email"
+              name="email"
+              placeholder="john@acme.com"
+              type="email"
+            />
+          </FormBlock>
+
+          <Button type="submit">Submit</Button>
+        </Form>
+      )}
     </Formik>
   );
 };
+
+const FormRadioButton = styled(Button)`
+  padding: 0;
+`;
 const FormLabel = styled.label`
   display: block;
   margin-bottom: 0.25rem;
