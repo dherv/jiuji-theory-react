ef7353fe6503f6fe72fdd8043dabb6c5222bf500
dherv <damien.herv@gmail.com>
2020-07-26 21:56:14 +0900

    chore(fonts): add WebFont loader

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,7 +2,13 @@ import React from "react";
 import { addDecorator } from "@storybook/react";
 import GlobalStyle from "../src/styled/global";
 import "destyle.css";
+import WebFont from "webfontloader";
 
+WebFont.load({
+  google: {
+    families: ["Saira Stencil One:300,400,700", "sans-serif"],
+  },
+});
 addDecorator((story) => (
   <>
     <GlobalStyle />
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,21 @@
 import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import WebFont from 'webfontloader';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+WebFont.load({
+  google: {
+    families: ["Saira Stencil One:300,400,700", "sans-serif"],
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
