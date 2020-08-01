50e196e90b2f537ad4796b0c44f6c5763017b507
dherv <damien.herv@gmail.com>
2020-08-01 14:36:59 +0900

    refactor(videos): rename VideoList component TechniqueVideoList

--- src/__tests__/TechniqueVideoList.test.tsx
+++ src/__tests__/TechniqueVideoList.test.tsx
@@ -1,10 +1,10 @@
 import 'jest-styled-components';
 import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
-import VideoList from '../components/VideoList';
-import { VideoListProps as props } from '../samples/VideoList.sample';
+import TechniqueVideoList from '../components/TechniqueVideoList';
+import { TechniqueVideoListProps as props } from '../samples/TechniqueVideoList.sample';
 
 test("should render multiple videos", () => {
-  render(<VideoList {...props} />);
+  render(<TechniqueVideoList {...props} />);
   expect(screen.queryAllByTitle("video title")).toHaveLength(3);
 });
