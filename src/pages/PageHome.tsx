5659be34c295ad3d19f5920f24e0daccf30df5b9
dherv <damien.herv@gmail.com>
2020-08-01 14:51:20 +0900

    refactor: change naming of styled tag in PageHome

--- src/pages/PageHome.tsx
+++ src/pages/PageHome.tsx
@@ -6,18 +6,18 @@ import TemplatePage from '../templates/TemplatePage';
 const PageHome: FC<{ techniques: any }> = ({ techniques }) => {
   return (
     <TemplatePage>
-      <StyledList>
+      <List>
         {techniques.map((technique: any) => (
           <li key={technique.id}>
             <Technique {...technique} />
           </li>
         ))}
-      </StyledList>
+      </List>
     </TemplatePage>
   );
 };
 
-const StyledList = styled.ul`
+const List = styled.ul`
   max-width: 440px;
   border: 1px solid rgba(18, 70, 246, 0.1);
 `;
