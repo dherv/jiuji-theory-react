0c6de051beddd9a74272f0ada6981655f2edb593
dherv <damien.herv@gmail.com>
2020-08-01 14:38:14 +0900

    chore(videos): add formik decorator to select story

--- src/stories/Select.stories.tsx
+++ src/stories/Select.stories.tsx
@@ -1,16 +1,23 @@
-
-import React from "react";
-import Select from "../components/Select";
-import { action } from "@storybook/addon-actions";
-import { SelectProps as propsData } from "../samples/Select.sample";
+import React from 'react';
+import withFormik from 'storybook-formik';
+import { action } from '@storybook/addon-actions';
+import Select from '../components/Select';
+import { SelectProps as propsData } from '../samples/Select.sample';
 
 export default {
   component: Select,
   title: "Select",
   excludeStories: /.*Data$/,
+  decorators: [withFormik],
+  parameters: {
+    formik: {
+      initialValues: {
+        query: "",
+      },
+    },
+  },
 };
 
 const actionsData = {};
 
-export const Default = () => <Select  {...propsData}  {...actionsData}/>;
-    
+export const Default = () => <Select {...propsData} {...actionsData} />;
